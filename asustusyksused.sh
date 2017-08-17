e684cd3edab7f0d17331a87ccc10ab102ed0eba2
Tormi Tabor <tormi.tabor@gmail.com>
2017-08-17 11:10:14 +0300

    Pindala m2 > km2 (#2)

--- asustusyksused.sh
+++ asustusyksused.sh
@@ -2,5 +2,5 @@
 
 curl -O "https://raw.githubusercontent.com/buildig/EHAK/master/geojson/asustusyksus.json" &&
 curl -O "https://raw.githubusercontent.com/buildig/EHAK/master/csv/asustusyksus.csv" &&
-mapshaper -i asustusyksus.json -filter '"0727,0728,0297,0517".indexOf(OKOOD) > -1' -o asustusyksused.json &&
-mapshaper -i asustusyksus.csv -filter '"0727,0728,0297,0517".indexOf(OKOOD) > -1' -sort AREA -o asustusyksused.csv
+mapshaper -i asustusyksus.json -filter '"0727,0728,0297,0517".indexOf(OKOOD) > -1' -each 'AREA=(AREA / 1000000).toFixed(2)' -o asustusyksused.json && 
+mapshaper -i asustusyksus.csv -filter '"0727,0728,0297,0517".indexOf(OKOOD) > -1' -each 'AREA=(AREA / 1000000).toFixed(2)' -sort AREA -o asustusyksused.csv
