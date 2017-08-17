2589a77d510fb35c32586415a4cf3ac3e763d933
Tormi Tabor <tormi.tabor@gmail.com>
2017-08-17 14:22:08 +0300

    Sortimine ANIMI järgi (#2)

--- asustusyksused.sh
+++ asustusyksused.sh
@@ -3,4 +3,4 @@
 curl -O "https://raw.githubusercontent.com/buildig/EHAK/master/geojson/asustusyksus.json" &&
 curl -O "https://raw.githubusercontent.com/buildig/EHAK/master/csv/asustusyksus.csv" &&
 mapshaper -i asustusyksus.json -filter '"0727,0728,0297,0517".indexOf(OKOOD) > -1' -each 'AREA=(AREA / 1000000).toFixed(2)' -o asustusyksused.json && 
-mapshaper -i asustusyksus.csv -filter '"0727,0728,0297,0517".indexOf(OKOOD) > -1' -each 'AREA=(AREA / 1000000).toFixed(2)' -sort AREA -o asustusyksused.csv
+mapshaper -i asustusyksus.csv -filter '"0727,0728,0297,0517".indexOf(OKOOD) > -1' -each 'AREA=(AREA / 1000000).toFixed(2)' -sort ANIMI -o asustusyksused.csv
